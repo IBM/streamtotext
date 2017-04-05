c1d68627f9b57340f12fce6d4c37b179a1a43d40
Gregory Haynes <greg@greghaynes.net>
2017-04-05 17:19:10 +0000

    Remove unused interruptable_get util

--- streamtotext/utils.py
+++ streamtotext/utils.py
@@ -1,36 +1,4 @@
-import asyncio
 import os
-
-
-class InterruptError(Exception):
-    pass
-
-
-async def interruptable_get(queue, event, loop=None):
-    loop = loop or asyncio.get_event_loop()
-    trigger = asyncio.Event()
-
-    def set_trigger(future):
-        trigger.set()
-
-    get_fut = asyncio.ensure_future(queue.get())
-
-    # fast path
-    if get_fut.done():
-        return get_fut.result()
-
-    get_fut.add_done_callback(set_trigger)
-
-    interrupt_fut = asyncio.ensure_future(event.wait())
-    interrupt_fut.add_done_callback(set_trigger)
-
-    await trigger.wait()
-
-    if get_fut.done():
-        interrupt_fut.cancel()
-        return get_fut.result()
-    get_fut.cancel()
-    raise InterruptError
 
 
 def wav_dir():
