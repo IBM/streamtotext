08f0c70914790dedff648302e9e77ba8afcec433
Gregory Haynes <greg@greghaynes.net>
2017-05-03 16:48:16 -0700

    Add libpulse-dev to bonnyci deps

--- .bonnyci/run.sh
+++ .bonnyci/run.sh
@@ -3,7 +3,7 @@
 set -eux
 set -o pipefail
 
-sudo apt-get install -y python3 python3-dev python3-pip swig portaudio19-dev
+sudo apt-get install -y python3 python3-dev python3-pip swig portaudio19-dev libpulse-dev
 sudo pip3 install tox
 
 tox -e py35
