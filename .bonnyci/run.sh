a5d51e2aadbc62f70c198ae4565a0283cc93df87
Jonathan Harker <jesusaurus@users.noreply.github.com>
2017-05-18 15:30:10 -0700

    Update apt cache before install packages
    Make sure the apt cache is up-to-date before installing packages

--- .bonnyci/run.sh
+++ .bonnyci/run.sh
@@ -3,6 +3,7 @@
 set -eux
 set -o pipefail
 
+sudo apt-get update
 sudo apt-get install -y python3 python3-dev python3-pip swig portaudio19-dev libpulse-dev pocketsphinx-en-us
 sudo pip3 install tox
 
